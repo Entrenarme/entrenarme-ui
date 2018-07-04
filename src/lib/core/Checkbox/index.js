5f81ccab932931139eecc373c911fcbc053104cd
Jordi Talens <talensjr@gmail.com>
2018-07-04 10:02:13 +0200

    Trying to change material colors

--- src/lib/core/Checkbox/index.js
+++ src/lib/core/Checkbox/index.js
@@ -1,5 +1,26 @@
 // @flow
-// import * as React from 'react';
+import * as React from 'react';
 import MCheckbox from '@material-ui/core/Checkbox';
+import { withStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
 
-export default MCheckbox;
+const styles = {
+  root: {
+    color: 'yellow',
+    '&$checked': {
+      color: 'red',
+    },
+  },
+  checked: {},
+};
+
+const Checkbox = ({ classes }) => (
+  <MCheckbox
+    classes={{
+      root: classes.root,
+      checked: classes.checked,
+    }}
+  />
+);
+
+export default withStyles(styles)(Checkbox);
