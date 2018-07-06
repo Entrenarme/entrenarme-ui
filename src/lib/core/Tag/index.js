8b8317131e36fa9b4b4383ed7567aeeb1ab25d81
Enric <enriccc15@gmail.com>
2018-07-06 09:35:37 +0200

    Corregida la fuente

--- src/lib/core/Tag/index.js
+++ src/lib/core/Tag/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import styled from 'styled-components';
 import Chip from '@material-ui/core/Chip';
 
-import { mainFont } from '../../helpers/fonts';
+import { regularFont } from '../../helpers/fonts';
+import colors from '../../helpers/colors';
 
 const ExtendedTag = styled(Chip)`
   height: 24px !important;
-  font-family: ${mainFont} !important;
+  font-family: ${regularFont} !important;
   font-size: 0.875rem !important;
+
+  span {
+    color: ${colors.text};
+  }
 
   @media only screen and (max-width: 767px) {
     font-size: 0.75rem !important;
