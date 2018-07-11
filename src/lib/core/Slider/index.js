691a7c819ceef934438d5d08ab207883585ca717
Enric <enriccc15@gmail.com>
2018-07-11 16:23:37 +0200

    Mas estilos

--- src/lib/core/Slider/index.js
+++ src/lib/core/Slider/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import ReactSlider from 'react-slick';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
 import './styles/slick-theme.css';
 import './styles/slick.css';
@@ -17,6 +18,15 @@ type ArrowProps = {
   onClick?: Function,
   icon: React.Node,
 };
+
+const StyledSlider = styled(ReactSlider)`
+  .slick-track {
+    width: ${props => (props.options.less ? null : '100% !important')};
+    display: ${props => (props.options.less ? null : 'flex !important')};
+    justify-content: ${props =>
+      props.options.less ? null : 'space-between !important'};
+  }
+`;
 
 const Arrow = ({ className, style, onClick, icon }: ArrowProps) => (
   <div
@@ -35,12 +45,19 @@ const Slider = ({ children, ...rest }: Props) => {
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    infinite: React.Children.count(children) >= 3 ? true : false,
+    infinite: React.Children.count(children) >= 3,
     nextArrow: <Arrow icon={faAngleRight} />,
     prevArrow: <Arrow icon={faAngleLeft} />,
   };
 
-  return <ReactSlider {...settings}>{children}</ReactSlider>;
+  return (
+    <StyledSlider
+      {...settings}
+      options={{ less: React.Children.count(children) >= 3 }}
+    >
+      {children}
+    </StyledSlider>
+  );
 };
 
 Slider.defaultProps = {};
