10faad3bd5cf8e2f0ec6310847551595b001632e
Enric <enriccc15@gmail.com>
2018-07-12 17:07:44 +0200

    InfoAlert

---
+++ src/lib/core/Alerts/InfoText/index.stories.js
@@ -0,0 +1,45 @@
+// @flow
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAlarmClock,
+  faAmbulance,
+  faAddressBook,
+} from '@fortawesome/pro-light-svg-icons';
+
+import InfoText from './index';
+
+storiesOf('Info text', module).add(
+  'success type',
+  withInfo('success type')(() => (
+    <InfoText
+      text="success text"
+      icon={<FontAwesomeIcon icon={faAlarmClock} />}
+      type="success"
+    />
+  )),
+);
+
+storiesOf('Info text', module).add(
+  'warning type',
+  withInfo('warning type')(() => (
+    <InfoText
+      text="error text"
+      icon={<FontAwesomeIcon icon={faAmbulance} />}
+      type="error"
+    />
+  )),
+);
+
+storiesOf('Info text', module).add(
+  'error type',
+  withInfo('error type')(() => (
+    <InfoText
+      text="warning text"
+      icon={<FontAwesomeIcon icon={faAddressBook} />}
+      type="warning"
+    />
+  )),
+);
