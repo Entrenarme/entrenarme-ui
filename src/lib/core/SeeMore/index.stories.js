e2752f9f652c513b86e9e2278264dab3eeab3ce1
Enric <enriccc15@gmail.com>
2018-07-16 17:18:44 +0200

    Componente seeMore

---
+++ src/lib/core/SeeMore/index.stories.js
@@ -0,0 +1,18 @@
+// @flow
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import SeeMore from './index';
+
+storiesOf('SeeMore', module).add(
+  'default',
+  withInfo('SeeMore')(() => (
+    <SeeMore maxChars={200} text={<div>ver más</div>}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+      magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
+      magna euismod et. Donec condimentum congue sapien, nec auctor eros
+      sagittis eget.
+    </SeeMore>
+  )),
+);
