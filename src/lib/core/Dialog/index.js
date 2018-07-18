a04a06934054cb272a95585f58485131e937d4b3
Enric <enriccc15@gmail.com>
2018-07-18 13:37:46 +0200

    Mas cambios y diseño mejorado

--- src/lib/core/Dialog/index.js
+++ src/lib/core/Dialog/index.js
@@ -1,37 +1,125 @@
 // @flow
 import * as React from 'react';
 import MDialog from '@material-ui/core/Dialog';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/pro-light-svg-icons';
 import styled from 'styled-components';
 
+import colors from '../../helpers/colors';
+
+const setDialogSize = type => {
+  switch (type) {
+    case 'large':
+      return '650px';
+    case 'small':
+      return '400px';
+    default:
+      return '550px';
+  }
+};
+
 const ExtendedDialog = styled(MDialog)`
-  > div > div {
-    width: 550px;
+  > div:nth-child(2) {
+    width: ${props => setDialogSize(props.options.type)};
+    padding: 30px;
+    border-radius: 4px;
+  }
+
+  @media only screen and (max-width: 767px) {
+    > div:nth-child(2) {
+      width: 100%;
+      padding: 20px;
+      margin: 10px;
+      border-radius: 4px;
+    }
+  }
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+
+  * {
+    margin-bottom: 0px;
+  }
+
+  svg {
+    font-size: 20px;
+    color: ${colors.primary.default};
+    cursor: pointer;
+  }
+`;
+
+const MiddleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  text-align: ${props =>
+    props.options.align === 'center' ? 'center' : 'initial'};
+  align-items: ${props =>
+    props.options.align === 'center' ? 'center' : 'initial'};
+`;
+
+const BodyContainer = styled.div``;
+
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  margin-top: 30px;
+
+  @media only screen and (max-width: 767px) {
+    flex-direction: column;
   }
 `;
 
 type Props = {
+  header?: React.Node,
+  body: React.Node,
+  footer?: React.Node,
   open: boolean,
   onClose: Function,
-};
-
-type State = {
-  open: boolean,
+  align?: 'default' | 'center',
+  type?: 'large' | 'default' | 'small',
 };
 
 class Dialog extends React.Component<Props, State> {
-  state = {
-    open: false,
-  };
-
   render() {
-    const { open, onClose } = this.props;
+    const {
+      header,
+      body,
+      footer,
+      open,
+      onClose,
+      type,
+      align,
+      ...rest
+    } = this.props;
 
     return (
-      <ExtendedDialog open={open} onClose={() => onClose()}>
-        bña
+      <ExtendedDialog
+        open={open}
+        onClose={() => onClose()}
+        options={{ type }}
+        {...rest}
+      >
+        <HeaderContainer>
+          {header ? header : <div />}
+          <FontAwesomeIcon icon={faTimes} onClick={() => onClose()} />
+        </HeaderContainer>
+        <MiddleContainer options={{ align }}>
+          <BodyContainer>{body}</BodyContainer>
+          {footer ? <FooterContainer>{footer}</FooterContainer> : null}
+        </MiddleContainer>
       </ExtendedDialog>
     );
   }
 }
+
+Dialog.defaultProps = {
+  align: 'default',
+  type: 'default',
+};
 
 export default Dialog;
--- src/lib/core/Dialog/index.stories.js
+++ src/lib/core/Dialog/index.stories.js
@@ -1,25 +1,91 @@
 // @flow
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Dialog from './index';
+import H4 from '../Text/Headers/H4/index';
+import Button from '../Button/index';
 
 let openDialog = false;
 
+const actions = {
+  onClick: action('onClick'),
+};
+
 storiesOf('Dialog', module).add('default', () => (
-  <div>
-    <button
-      onClick={() => {
-        openDialog = !openDialog;
-      }}
-    >
-      Open dialog
-    </button>
-    <Dialog open={true} onClose={() => (openDialog = false)}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+  <Dialog
+    {...actions}
+    open={true}
+    onClose={() => (openDialog = false)}
+    header={<H4>TEST</H4>}
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
       magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
       magna euismod et. Donec condimentum congue sapien, nec auctor eros
-      sagittis eget.
-    </Dialog>
-  </div>
+      sagittis eget."
+    footer={
+      <div>
+        <Button>Test</Button>
+        <Button>Test</Button>
+      </div>
+    }
+  />
+));
+
+storiesOf('Dialog', module).add('large', () => (
+  <Dialog
+    {...actions}
+    open={true}
+    onClose={() => (openDialog = false)}
+    header={<H4>TEST</H4>}
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+      magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
+      magna euismod et. Donec condimentum congue sapien, nec auctor eros
+      sagittis eget."
+    footer={<Button>Test</Button>}
+    type="large"
+  />
+));
+
+storiesOf('Dialog', module).add('small', () => (
+  <Dialog
+    {...actions}
+    open={true}
+    onClose={() => (openDialog = false)}
+    header={<H4>TEST</H4>}
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+      magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
+      magna euismod et. Donec condimentum congue sapien, nec auctor eros
+      sagittis eget."
+    footer={<Button>Test</Button>}
+    type="small"
+  />
+));
+
+storiesOf('Dialog', module).add('with centered content', () => (
+  <Dialog
+    {...actions}
+    open={true}
+    onClose={() => (openDialog = false)}
+    header={<H4>TEST</H4>}
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+      magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
+      magna euismod et. Donec condimentum congue sapien, nec auctor eros
+      sagittis eget."
+    footer={<Button>Test</Button>}
+    align="center"
+  />
+));
+
+storiesOf('Dialog', module).add('without header', () => (
+  <Dialog
+    {...actions}
+    open={true}
+    onClose={() => (openDialog = false)}
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum
+      magna eu velit semper hendrerit. Aenean aliquet pretium justo, ut rhoncus
+      magna euismod et. Donec condimentum congue sapien, nec auctor eros
+      sagittis eget."
+    footer={<Button>Test</Button>}
+  />
 ));
