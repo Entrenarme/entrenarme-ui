74b6f3d0b5aff4d1cbd187861dc9244a06abff47
Enric <enriccc15@gmail.com>
2018-07-24 15:48:07 +0200

    Mas cambios

--- src/lib/core/Tabs/index.js
+++ src/lib/core/Tabs/index.js
@@ -1,16 +1,75 @@
 // @flow
 import * as React from 'react';
+import styled from 'styled-components';
+
+import { mainFont } from '../../helpers/fonts';
+import colors from '../../helpers/colors';
+
+const MainTabContainer = styled.div`
+  display: flex;
+`;
+
+const TabContainer = styled.div`
+  font-family: ${mainFont};
+  font-size: 0.875rem;
+  padding: 20px 30px;
+  cursor: pointer;
+  height: 48px;
+  color: ${props =>
+    props.options.isActive ? colors.secondary.default : colors.primary.default};
+  border-bottom: ${props =>
+    props.options.isActive
+      ? `2px solid ${colors.secondary.default}`
+      : colors.primary.default};
+
+  &:hover {
+    color: ${colors.secondary.default};
+  }
+`;
+
+const HorizontalSeparator = styled.hr`
+  display: block;
+  height: 1px;
+  margin: -2px
+  border: 0;
+  border-top: 2px solid ${colors.gray.light};
+  padding: 0;
+`;
 
 type Props = {
   children: React.Node,
 };
 
-type State = {};
+type State = {
+  selectedTab: ?number,
+};
+
+type TabProps = {
+  children: React.Node,
+  setSelectedContent: Function,
+  indexElement: number,
+  selectedTab: ?number,
+};
+
+const Tab = ({
+  children,
+  setSelectedContent,
+  indexElement,
+  selectedTab,
+}: TabProps) => {
+  return (
+    <TabContainer
+      onClick={() => setSelectedContent(indexElement)}
+      options={{ isActive: indexElement === selectedTab }}
+    >
+      {children}
+    </TabContainer>
+  );
+};
 
 class SwitchTab extends React.Component<Props, State> {
   state = {
     selectedTab: null,
-    selectedContent: null,
   };
 
   setSelectedContent = element =>
@@ -20,24 +79,31 @@ class SwitchTab extends React.Component<Props, State> {
 
   render() {
     const { children } = this.props;
+    const { selectedTab } = this.state;
 
     return (
       <div>
-        <div onClick={() => console.log()}>
-          {React.Children.map(children[0].props.children, child => {
-            return React.cloneElement(child, {
-              selectedTab: this.state.selectedTab,
-            });
-          })}
-        </div>
-        <hr />
-        <div>
-          {React.Children.map(children[1].props.children, child => {
-            return React.cloneElement(child, {
-              selectedContent: this.state.selectedContent,
-            });
-          })}
-        </div>
+        <MainTabContainer>
+          {React.Children.map(children[0].props.children, (child, index) => (
+            <Tab
+              setSelectedContent={this.setSelectedContent}
+              selectedTab={selectedTab}
+              indexElement={index}
+            >
+              {child}
+            </Tab>
+          ))}
+        </MainTabContainer>
+        <HorizontalSeparator />
+        {React.Children.map(children[1].props.children, (child, index) => (
+          <div
+            style={{
+              display: selectedTab === index ? 'block' : 'none',
+            }}
+          >
+            {child}
+          </div>
+        ))}
       </div>
     );
   }
--- src/lib/core/Tabs/index.stories.js
+++ src/lib/core/Tabs/index.stories.js
@@ -2,18 +2,53 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { RESOURCES_URL } from '../../helpers/config';
+
 import SwitchTab from './index';
 
-storiesOf('Tabs', module).add('default', () => (
+const imageURLTest = 'xB1sQl1TuP0yFj7qn8gb_sebas-b.jpg';
+
+storiesOf('Tabs', module).add('text tabs', () => (
   <SwitchTab>
     <div>
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
+      <p>Tab 1</p>
+      <p>Tab 2</p>
+      <p>Tab 3</p>
     </div>
     <div>
       <p>bla1</p>
-      <p>bla2</p>
+      <div>
+        <p>bla2</p>
+      </div>
+      <p>bla3</p>
+    </div>
+  </SwitchTab>
+));
+
+storiesOf('Tabs', module).add('image tabs', () => (
+  <SwitchTab>
+    <div>
+      <img
+        src={`${RESOURCES_URL}profile/150/${imageURLTest}`}
+        style={{ width: '50px', height: '50px', borderRadius: '50px' }}
+        alt="profile"
+      />
+      <img
+        src={`${RESOURCES_URL}profile/150/${imageURLTest}`}
+        style={{ width: '50px', height: '50px', borderRadius: '50px' }}
+        alt="profile"
+      />
+      <img
+        src={`${RESOURCES_URL}profile/150/${imageURLTest}`}
+        style={{ width: '50px', height: '50px', borderRadius: '50px' }}
+        alt="profile"
+      />
+    </div>
+    <div>
+      <p>bla1</p>
+      <div>
+        <p>bla2</p>
+      </div>
       <p>bla3</p>
     </div>
   </SwitchTab>
