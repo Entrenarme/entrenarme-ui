1eca048e6db451a6316421a08d7f5ccedffece36
Jordi Talens <talensjr@gmail.com>
2018-07-25 17:13:10 +0200

    Added rateadv desktop

--- src/lib/listing/RateAdv/index.js
+++ src/lib/listing/RateAdv/index.js
@@ -3,11 +3,26 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 import colors from '../../helpers/colors';
+import P from '../../core/Text/Paragraph';
+import Button from '../../core/Button';
+
+const Wrapper = styled.div`
+  width: 130px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Container = styled.div`
   background-color: ${colors.gray.light};
   width: 130px;
   height: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
 `;
 
 type Props = {
@@ -17,8 +32,44 @@ type Props = {
   currency: string,
 };
 
+const Price = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+`;
+
 const RatesAdv = ({ topText, bottomText, amount, currency }: Props) => (
-  <Container />
+  <Wrapper>
+    <Container>
+      <P size="xs" color={colors.primary.default} textAlign="center" noMargin>
+        {topText}
+      </P>
+      <Price>
+        <P
+          size="large"
+          color={colors.primary.default}
+          font="main"
+          textAlign="center"
+          noMargin
+        >
+          {amount}
+        </P>
+        <P
+          size="medium"
+          color={colors.primary.default}
+          font="main"
+          textAlign="center"
+          noMargin
+        >
+          {currency}
+        </P>
+      </Price>
+      <P size="xs" color={colors.primary.default} textAlign="center" noMargin>
+        {bottomText}
+      </P>
+    </Container>
+    <Button style={{ marginTop: '-2px' }}>1a sesión gratis</Button>
+  </Wrapper>
 );
 
 export default RatesAdv;
--- src/lib/listing/RateAdv/index.stories.js
+++ src/lib/listing/RateAdv/index.stories.js
@@ -4,4 +4,11 @@ import { storiesOf } from '@storybook/react';
 
 import RatesAdv from './index';
 
-storiesOf('RatesAdv', module).add('default', () => <RatesAdv />);
+storiesOf('RatesAdv', module).add('default', () => (
+  <RatesAdv
+    currency="€"
+    amount={12}
+    topText="desde"
+    bottomText="persona/sesión"
+  />
+));
