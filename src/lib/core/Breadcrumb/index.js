16c4aef8961f1eedcc258f185520f07fe58769d7
Enric <enriccc15@gmail.com>
2018-07-25 13:11:13 +0200

    Iniciado el componente Breadcrumbs

---
+++ src/lib/core/Breadcrumb/index.js
@@ -0,0 +1,77 @@
+// @flow
+import * as React from 'react';
+import styled from 'styled-components';
+
+import colors from '../../helpers/colors';
+import { regularFont } from '../../helpers/fonts';
+
+const MainContainer = styled.div`
+  display: flex;
+`;
+
+const LinkContainer = styled.div`
+  cursor: ${props => (props.options.disabled ? 'default' : 'pointer')};
+  color: ${props =>
+    props.options.disabled ? colors.gray.default : colors.gray.placeholder};
+  font-family: ${regularFont};
+  font-size: 1rem;
+  margin-right: 10px;
+
+  * {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    color: ${props =>
+      props.options.disabled ? colors.gray.default : colors.secondary.default};
+  }
+
+  &::after {
+    content: '/';
+    color: ${colors.gray.placeholder};
+  }
+
+  &:last-child {
+    cursor: default;
+    color: ${colors.text};
+    margin-right: 0px;
+
+    * {
+      margin-right: 0px;
+    }
+  }
+
+  &:last-child::after {
+    content: '';
+  }
+
+  /* iPhone 5/6/6+ ----------- */
+  @media only screen and (max-width: 767px) {
+    font-size: 0.875rem;
+    margin-right: 15px;
+
+    * {
+      margin-right: 15px;
+    }
+  }
+`;
+
+type Props = {
+  children: React.Node,
+};
+
+const Breadcrumb = ({ children }: Props) => {
+  return (
+    <MainContainer>
+      {React.Children.map(children, child => {
+        return (
+          <LinkContainer options={{ disabled: child.props.disabled }}>
+            {child}
+          </LinkContainer>
+        );
+      })}
+    </MainContainer>
+  );
+};
+
+export default Breadcrumb;
