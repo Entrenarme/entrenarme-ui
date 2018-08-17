c829b92b3546a977aae6079f79be22325b518d34
Jordi Talens <talensjr@gmail.com>
2018-08-17 17:21:49 +0200

    Updated test suite

---
+++ jest.setup.js
@@ -0,0 +1,13 @@
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
+
+global.requestAnimationFrame = cb => {
+  setTimeout(cb, 0);
+};
